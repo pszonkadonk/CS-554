ea29105a6ec006849a40b81430064f1528e93bc8
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-07-16 01:33:41 -0400

    Method to limit to 20, keep commented version

--- Lab5/routes/people.js
+++ Lab5/routes/people.js
@@ -8,29 +8,50 @@ const client = redis.createClient();
 const flat = require('flat');
 const unflatten = flat.unflatten;
 
+let recentVisitors = [];
+let recentHistory = [];
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 
 router.get('/history', async (req, res) => {
-    let recentlyVisited = [];
-     client.keys("*", (err, allKeys) => {
-         console.log(allKeys)
-         client.mget(allKeys, redis.print);
-      })
-        .exec((err, replies) => {
-            console.log("hello from exec");
-            if(err) {
-                console.log(err);
-            }
-            console.log(replies);
-        });
+    console.log("RECENT VISITORS");
+    console.log(recentVisitors);
+    if(recentVisitors.length < 20) {
+        for(let i = 0; i < recentVisitors.length; i++) {
+            let recentPerson = await client.hgetallAsync(recentVisitors[i].toString());
+            recentHistory.unshift(recentPerson);
+        }
+    } else {
+        for(let i = 0; i < 20; i++) {
+            let recentPerson = await client.hgetallAsync(recentVisitors[i].toString());
+            recentHistory.unshift(recentPerson);
+        }
+    }
+    res.send(recentHistory.reverse());
+    recentHistory = [];
+    // let m = client.multi();
+    // client.keys("*", (err, allKeys) => {
+    //     if(allKeys.length <= 20) {
+    //         for(let i = 0; i < allKeys.length; i++){
+    //             m.hgetallAsync(allKeys[i]);
+    //         }
+    //     }
+    //     else {
+    //         for(let i = 0; i < 20; i++) {
+    //             m.hgetallAsync(allKeys[i]);
+    //         }
+    //     }
+    //     m.exec((err, result) => {
+    //         res.send(result);
+    //     });
+    // });
 });
 
  
 router.get('/:id', async (req, res) => {
-    let person = await client.hgetallAsync(req.params.id)
+    let person = await client.hgetallAsync(req.params.id.toString());
     let target = null
     console.log(typeof(person));
     if(person == null) {
@@ -40,15 +61,15 @@ router.get('/:id', async (req, res) => {
             if(target) {
                 let flatPerson = flat(target);
                 let hmSetAsyncPerson = await client.hmsetAsync(target.id.toString(), flatPerson);
-                client.ltrim(20);
+                recentVisitors.unshift(target.id.toString());
                 res.json(target);
             }
         } catch(err) {
            res.send("Sorry, that person is not in the database")
         }
     } else {
+        recentVisitors.unshift(person.id.toString());
         let unflattenedPerson = unflatten(person);
-        client.ltrim(20);
         res.send(unflattenedPerson);
     }
 });
