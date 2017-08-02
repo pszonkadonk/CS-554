3a998e27836d76fa2691097c042833866264078e
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-08-01 23:31:03 -0400

    configure index.html

--- Lab7/app.js
+++ Lab7/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const staticFiles = express.static(__dirname + '/public');
 
 
 app.use(bodyParser.json());
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 
-app.use('/public', express.static(__dirname + "/public"));
+app.use(staticFiles);
 app.use(express.static(__dirname + "/views"));
 
 const configRoutes = require('./routes');
